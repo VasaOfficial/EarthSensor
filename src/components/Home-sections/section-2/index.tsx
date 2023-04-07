4483627f7afd369847ec304801c3d3d6f041f1b0
VasaOfficial <VasaPofficial@gmail.com>
2023-04-07 15:20:19 +0200

    feat: add text and image content to the section2

--- src/components/Home-sections/section-2/index.tsx
+++ src/components/Home-sections/section-2/index.tsx
@@ -1,5 +1,6 @@
 import Image from "next/image"
 import { Collapse, Text, Grid } from '@nextui-org/react';
+import City from 'public/assets/polluted-city.jpg'
 
 
 export default function Section2() {
@@ -28,33 +29,103 @@ export default function Section2() {
           </span>
         </p>
         <div className='flex flex-col md:flex-row justify-center items-center md:items-start gap-12'>
-          <figure className='relative h-52 md:w-72 w-[90%] px-[5vw]'>
+          <figure className='relative h-96 md:w-72 w-[90%] px-[5vw] '>
+            <Image
+              src={City}
+              alt=''
+              role='presentation'
+              fill
+              priority={true}
+              className='max-w-full max-h-full rounded-3xl'
+            />
           </figure>
           <Grid.Container gap={2}>
       <Grid>
         <Collapse.Group splitted>
-          <Collapse title="Option A">
+          <Collapse title="Industries & heating">
             <Text>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
-              enim ad minim veniam, quis nostrud exercitation ullamco laboris
-              nisi ut aliquip ex ea commodo consequat.
+            <p>
+                The combustion of fossil fuels such as coal and oil in
+                industrial processes in power plants, refineries, and factories
+                release a variety of pollutants, the majority of which are
+                identical to those emitted by traffic and mobility.
+              </p> <br />
+              <p>
+                On top of this, chemical processes and volatile industry
+                byproducts also cause VOC emissions. In Europe, around 60% of
+                sulfur oxides come from energy production and distribution. In
+                the US, stationary fuel combustion sources like electric
+                utilities and industrial boilers are responsible for 73.2% of
+                sulfur dioxide pollution.
+              </p>
             </Text>
           </Collapse>
-          <Collapse title="Option B">
+          <Collapse title="Traffic & mobility">
             <Text>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
-              enim ad minim veniam, quis nostrud exercitation ullamco laboris
-              nisi ut aliquip ex ea commodo consequat.
+            <p>
+                Petrol and diesel engines of cars, ships, trains and other
+                vehicles emit pollutants such as carbon monoxide (CO), nitrogen
+                oxides (NOx), particulate matter (PM), sulfur dioxide (SO2), and
+                volatile organic compounds (VOCs).
+              </p> <br />
+              <p>
+                Friction from tires and brake wear also create primary – i.e.
+                direct – particulate matter emissions. In addition, the nitrogen
+                dioxide (NO2) and VOCs released by road vehicles also undergo
+                photochemical reactions to form ozone (O3).
+              </p> <br />
+              <p>
+                In Europe, more than 40% of NOx and almost 40% of primary PM2.5
+                emissions are linked to road transport. In the United States,
+                35.8% of CO and 32.8% of NOx stem from road transport.
+              </p>
             </Text>
           </Collapse>
-          <Collapse title="Option C">
+          <Collapse title="Agriculture">
             <Text>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
-              enim ad minim veniam, quis nostrud exercitation ullamco laboris
-              nisi ut aliquip ex ea commodo consequat.
+            <p>
+                A wide range of nitrogen compounds (NO, NO2, N2), including
+                ammonia (NH3), can be attributed to fertilizer production, farm
+                machinery, and livestock waste management in agriculture. In
+                addition, methane (CH4) is released by the digestive processes
+                of livestock.
+              </p> <br />
+              <p>
+                In Europe, agricultural activities cause approximately 90% of
+                ammonia emissions and 80% of methane emissions. In the US,
+                livestock and manure management are responsible for 46% of
+                methane emissions. transport.
+              </p>
+            </Text>
+          </Collapse>
+          <Collapse title="Burning of Fossil Fuels">
+            <Text>
+            <p>
+                Sulfur dioxide emitted from the combustion of fossil fuels like
+                coal, petroleum for energy in power plants, and other factory
+                combustibles is one the major cause of air pollution. Billions
+                of vehicles run on roads are powered by gasoline and diesel
+                engines that burn petroleum for releasing energy. Petroleum is
+                made up of hydrocarbons, and engines don’t burn them cleanly.
+              </p> <br />
+              <p>
+                As a result, pollutants such as PM, nitric oxide and NO2
+                (together referred to as NOx), carbon monoxide, organic
+                compounds, and lead emit from vehicles including trucks, jeeps,
+                cars, trains, airplanes, causing a high level of pollution.
+                These modes of transportation form part of our daily basic
+                needs, so we rely on them heavily. But, their overuse is killing
+                our environment as dangerous gases are polluting the atmosphere.
+              </p> <br />
+              <p>
+                Carbon Monoxide caused by improper or incomplete combustion and
+                generally emitted from vehicles is another major pollutant along
+                with Nitrogen Oxides, that is produced from both natural and
+                man-made processes. As per the World Health Organization (WHO),
+                exposure to outdoor air pollution contributes to as much as 0.6
+                to 1.4 percent of the burden of disease and 4.2 million deaths
+                every year.
+              </p>
             </Text>
           </Collapse>
         </Collapse.Group>
