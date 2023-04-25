639d4d8701f9ba134a50f5bf7c195d5dbe085ce1
VasaOfficial <VasaPofficial@gmail.com>
2023-04-25 18:58:37 +0200

    feat: add slow rotation to the 3d globe

--- src/components/Earth/index.jsx
+++ src/components/Earth/index.jsx
@@ -1,21 +1,47 @@
+import { useState, useEffect } from 'react';
 import Map from 'react-map-gl';
+
 import { env } from '~/env.mjs';
 
 export default function Map3d() {
+  const [viewState, setViewState] = useState({
+    longitude: 20.568829,
+    latitude: 11.612623,
+    zoom: 1.61,
+  });
+  const [isRotating, setIsRotating] = useState(false);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setViewState((prev) => ({
+        ...prev,
+        longitude: prev.longitude + 0.2, // Adjust the rotation speed here
+      }));
+    }, 50); // Adjust the interval here
+
+    return () => clearInterval(interval);
+  }, []);
+
+  const handleMove = (event) => {
+    setIsRotating(event.dragging);
+    if (!event.dragging && !isRotating) {
+      setViewState(event.viewState);
+    }
+  };
+
   return (
     <Map
-      initialViewState={{
-        longitude: 20.568829,
-        latitude: 11.612623,
-        zoom: 1.61
-      }}
-      style={{width: 800, height: 500}}
+      {...viewState}
+      style={{ width: 800, height: 500 }}
       mapStyle="mapbox://styles/mapbox/satellite-v9"
       mapboxAccessToken={env.NEXT_PUBLIC_MAPBOX_KEY}
-      projection={'globe'}
+      dragRotate={true}
+      onLoad={() => setIsRotating(false)}
+      onMove={handleMove}
       attributionControl={false}
-      touchPitch={false}
       logoPosition={'bottom-right'}
+      projection={'globe'}
+      scrollZoom={false}
     />
-  )
+  );
 }
