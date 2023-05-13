0943d1a1dd3c2674ae081ade9353e88af3175152
VasaOfficial <VasaPofficial@gmail.com>
2023-05-13 21:00:00 +0200

    style: change the error page

--- app/error.tsx
+++ app/error.tsx
@@ -1,6 +1,10 @@
 'use client'; // Error components must be Client components
 
-import { useEffect } from 'react';
+import Link from 'next/link';
+import { Inconsolata } from 'next/font/google'
+import './styles/error.css'
+
+const inconsolata = Inconsolata({ subsets: ['latin'] });
 
 const Error = ({
   error,
@@ -9,24 +13,19 @@ const Error = ({
   error: Error;
   reset: () => void;
 }) => {
-  useEffect(() => {
-    // Log the error to an error reporting service
-    console.error(error);
-  }, [error]);
 
   return (
-    <div>
-      <h2>Something went wrong!</h2>
-      <button
-        onClick={
-          // Attempt to recover by trying to re-render the segment
-          () => reset()
-        }
-      >
-        Try again
-      </button>
-    </div>
+    <>
+      <div className="noise"></div>
+      <div className="overlay"></div>
+      <div className={`${inconsolata.className} terminal`}>
+        <h1>Error <span className="errorcode">404</span></h1>
+        <p className="output">The page you are looking for might have been removed, had its name changed or is temporarily unavailable.</p>
+        <p className="output">Please try <Link href="/">again</Link></p>
+        <p className="output">Good luck.</p>
+      </div>
+    </>
   );
-}
+  }
 
 export default Error;
